be3aeb60f3f2219cefc3acfe609d03961a34f1ec
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2023-08-08 11:19:27 +0200

    f

--- script/script.js
+++ script/script.js
@@ -18,3 +18,56 @@ document.querySelector('#copy-hash').onclick = function(){
   copy_hash(document.querySelector('#hash'))
 }
 
+
+document.addEventListener('DOMContentLoaded', function() {
+  var leftArrow = document.querySelectorAll('.scroll-left');
+  var rightArrow = document.querySelectorAll('.scroll-right');
+
+  leftArrow.forEach(left_arrow => {
+    console.log(left_arrow.parentElement)
+    left_arrow.addEventListener('click', function() {
+      var contentContainer = this.parentElement
+      var scrollAmount = contentContainer.clientWidth / 2;
+      contentContainer.scrollTo({
+        left: contentContainer.scrollLeft - scrollAmount,
+        behavior: 'smooth'
+      });
+    });
+  });
+  rightArrow.forEach(right_arrow => {
+    console.log(right_arrow.parentElement)
+    right_arrow.addEventListener('click', function() {
+      var contentContainer = this.parentElement
+      var scrollAmount = contentContainer.clientWidth / 2;
+      contentContainer.scrollTo({
+        left: contentContainer.scrollLeft + scrollAmount,
+        behavior: 'smooth'
+      });
+    });
+  });
+
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var contentContainer = document.querySelector('.flex_container_steps');
+//   var leftArrow = document.querySelector('.scroll-left');
+//   var rightArrow = document.querySelector('.scroll-right');
+
+//   console.log(contentContainer.clientWidth)
+  
+//   leftArrow.addEventListener('click', function() {
+//     var scrollAmount = contentContainer.clientWidth / 2; // Amount of scroll in pixls
+//     contentContainer.scrollTo({
+//       left: contentContainer.scrollLeft - scrollAmount,
+//       behavior: 'smooth'
+//     });
+//   });
+  
+//   rightArrow.addEventListener('click', function() {
+//     var scrollAmount = contentContainer.clientWidth / 2; // Amount of scroll in pixls
+//     contentContainer.scrollTo({
+//       left: contentContainer.scrollLeft + scrollAmount,
+//       behavior: 'smooth'
+//     });
+//   });
+// });
